e8d16b3e8ce77c620a3782a8083e7eaf9d9db55c
ShripriyaR <shripriyaravi@gmail.com>
2024-04-23 14:54:42 +0530

    update version

--- schema/userSchema.js
+++ schema/userSchema.js
@@ -2,7 +2,7 @@ import mongoose from "../config/dbconfig.js";
 
 
 const appSchema = new mongoose.Schema({
-    name: String,
+    username: String,
     password: String,
     icon: String,
     // Add additional fields like created date, last modified date if needed
